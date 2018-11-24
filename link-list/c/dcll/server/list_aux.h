e5579af89e8749c14289282ce819d1391b5c2f03
Prabhakar Patil <prpatil12@gmail.com>
2018-11-24 09:29:45 +0530

    list_aux.h comments modified

--- link-list/c/dcll/server/list_aux.h
+++ link-list/c/dcll/server/list_aux.h
@@ -11,11 +11,12 @@
 /* auxillary functions/routines */
 
 node_t *get_node (data_t d); 	/* creates one node dynamically in memory, copies data in data field
-				and returns base address of node (struct node)declarations */
+				and returns base address of node (struct node) */
 	
-bool_t g_insert (node_t *beg, node_t *mid, node_t *end); 	/* generic insert mid inbetween beg and end node, 
+bool_t g_insert (node_t *beg, node_t *mid, node_t *end); 	/* generic routine, insert mid node  in between beg and end nodes, 
 								used by insert_*() interface routines */
-bool_t g_delete (node_t *del_node);	/*generic delete, used by del_*() insterface routines */
+
+bool_t g_delete (node_t *del_node);	/*generic delete, used by del_*() interface routines */
 node_t *g_search_node (node_t *lst, data_t s_data);	/*generic search for data, linear search through list*/
 
 #endif /*_LIST_AUX_H_*/
